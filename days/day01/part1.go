bddbc60904b13915d5a94bb89d4b517a216eef2b
Alex Potter <git@alexpotter.dev>
2025-12-01 09:03:34 +0000

    Day 01 Part 01

---
+++ days/day01/part1.go
@@ -0,0 +1,52 @@
+// Package day01 contains solutions for the Day 1 problems
+package day01
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func Solve() int {
+	file, err := os.Open("days/day01/input.txt")
+	if err != nil {
+		panic("Failed to open input file: " + err.Error())
+	}
+
+	defer file.Close()
+
+	password := 0
+	var position int64 = 50
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		direction := line[0:1]
+		turns, err := strconv.ParseInt(line[1:], 10, 16)
+		if err != nil {
+			panic("Failed to parse number of turns: " + err.Error())
+		}
+
+		switch direction {
+		case "L":
+			{
+				// The modulo operator can return negative numbers
+				position = (((position - turns) % 100) + 100) % 100
+				break
+			}
+		case "R":
+			{
+				position = (position + turns) % 100
+				break
+			}
+		}
+
+		if position == 0 {
+			password++
+		}
+	}
+
+	return password
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"ajp/aoc25/days/day01"
+)
+
+func main() {
+	solution := day01.Solve()
+	fmt.Println("The solution is:", solution)
+}
